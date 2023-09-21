6a11dc999d332a1e21d9580ca19e2a681a93e1de
Ginés Cruz Chávez <gines.cruz.30@ull.edu.es>
2023-09-21 11:21:52 +0100

    Add files via upload

---
+++ DecirNombre.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DecirNombre : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log("Hola, soy " + gameObject.name);
+    }
+}
